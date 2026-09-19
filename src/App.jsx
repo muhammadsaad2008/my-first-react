aee0c9ccb1c2a8cc27f03ce4b9466248da5c943b
muhammadsaad2008 <saad2008.3y@gmail.com>
2026-09-19 18:51:07 +0500

    react folder structure

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+function App() {
+  return (
+    <div>
+      <div>This is first react App </div>
+      <ol>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ol>
+     <table>
+    <thead><tr>
+      <th>Name</th>
+      <th>class</th>
+      <th>Phone No</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Saad</td>
+          <td>12th</td>
+          <td>0325461989</td>
+        </tr>
+        <tr>
+            <td>Ali</td>
+            <td>12th</td>
+            <td>02498248+</td>
+        </tr>
+      </tbody>
+     </table>
+     <form action="">
+      <input type="text" placeholder="Enter Text" />
+      <input type="number" enter number />
+      <button type="submit" >click</button>
+
+     </form>
+    </div>
+  )
+}
+
+export default App;
