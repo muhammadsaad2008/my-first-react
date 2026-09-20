0ce5173821f26979c29524e45b82b7019f34dcc5
muhammadsaad2008 <saad2008.3y@gmail.com>
2026-09-20 18:52:28 +0500

    Today we learn state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,43 @@
-function App() {
-  return (
+import {useState} from "react";
+function App(){
+  const [count,setCount]=useState(5);
+  const [ObtMarks, setObtMarks] = useState(0);
+  const [TotMarks, setTotMarks] = useState(0);
+  const [Percentange, setPercantage] = useState(0);
+  function increment(){
+    setCount(count+1)
+  }
+  function Decrement() {
+    if (count > 0) {
+      setCount(count - 1);  
+    }
+    
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const CalPercantage=(ObtMarks/TotMarks)*100;
+    setPercantage(CalPercantage);
+    // alert(`Percantage:${CalPercantage.toFixed(2)}%`);
+  }
+  return(
     <div>
-      <div>This is first react App </div>
-      <ol>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-      </ol>
-     <table>
-    <thead><tr>
-      <th>Name</th>
-      <th>class</th>
-      <th>Phone No</th>
-      </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Saad</td>
-          <td>12th</td>
-          <td>0325461989</td>
-        </tr>
-        <tr>
-            <td>Ali</td>
-            <td>12th</td>
-            <td>02498248+</td>
-        </tr>
-      </tbody>
-     </table>
-     <form action="">
-      <input type="text" placeholder="Enter Text" />
-      <input type="number" enter number />
-      <button type="submit" >click</button>
-
-     </form>
+     <div>This is counter variable </div>
+     <h1> Counter : {count} </h1>
+     <button onClick={increment}> Increment</button>
+    <button onClick={Decrement}> Decrement</button>
+      <form onSubmit={handleSubmit}>
+      <div>
+          <input type="Number" name="Obtain Marks" id="ObtMarks" placeholder="Enter Obtain Marks" 
+          onChange={(e)=>setObtMarks(e.target.value)}/>
+        </div>
+        <div>
+      <input type="Number" name="Obtain Marks" id="TotMarks" placeholder="Enter Total Marks" 
+      onChange={(e)=>setTotMarks(e.target.value)}/>
+        </div>
+      <button type="Submit" >Calculate</button>
+    </form>
+      <p>Your Calculated Percentage is {Percentange.toFixed(2)}%</p>
     </div>
   )
 }
-
 export default App;
